473c3b171d717ed17e7a75ed709ee0e8d09bd1b6
Electric <electriceagle@gmx.de>
2022-11-02 09:38:01 +0100

    first commit

---
+++ leaderboard/leaderBoard.js
@@ -0,0 +1,92 @@
+const returnQueryParams = () => {
+  const Location = document.location;
+  //returns the id as an object of this
+  const queryParams = new URLSearchParams(Location.search);
+
+  let eventId = "";
+  let anzahl = "";
+
+  if (queryParams != null) {
+    for (const [key, value] of queryParams) {
+      //Here will be a few other security tags
+      if (key == "eventId") {
+        eventId = value;
+      } else if (key == "anzahl") {
+        anzahl = value;
+      } else {
+        alert("Misinput at the query Params");
+      }
+    }
+  }
+
+  return [eventId, anzahl];
+};
+
+const createUrl = (eventId) => {
+  return `https://racemap.com/api/data/v1/${eventId}/ranks`;
+};
+
+const getLeaders = async (url) => {
+  /**"starters" */
+  let arrayRanks = [];
+  const request = await fetch(url);
+  const toJSon = await request.json();
+
+  for (starter of toJSon.starters) {
+    const rank = await starter.rank;
+    const name = await starter.name;
+
+    if (rank != -1) {
+      const starterObject = createObject(rank, name);
+
+      arrayRanks.push(starterObject);
+    } else {
+      const starterObject = createObject("Ausgeschieden", name);
+
+      arrayRanks.push(starterObject);
+    }
+  }
+  console.log(arrayRanks);
+  return arrayRanks;
+};
+
+const reload = (callback) => {
+  setInterval(callback, 1000);
+};
+
+const printOut = (value) => {
+  console.log(value);
+};
+
+const createAnzahlPTags = (anzahl) => {
+  for (i = 1; i < anzahl + 1; i++) {
+    const tag = document.createElement("h1");
+    tag.setAttribute("id", `${i}`);
+    const container = document.getElementById("container");
+    container.appendChild(tag);
+  }
+};
+
+const showToScreen = (starterobject, anzahl) => {
+  for (i = 0; i < anzahl; i++) {
+    let text = `${starterobject[i].Rank}: ${starterobject[i].Name}`;
+    document.getElementById(`${i + 1}`).innerHTML = text;
+  }
+};
+
+//simple function for creating an object that is stored inside arrayRanks
+const createObject = (rank, name) => {
+  let starterObject = {};
+  starterObject["Rank"] = rank;
+  starterObject["Name"] = name;
+  return starterObject;
+};
+
+const callbackFunction = (urlCreate, anzahl) => {
+  getLeaders(urlCreate).then((value) => showToScreen(value, anzahl));
+};
+
+const url = createUrl(returnQueryParams()[0]);
+const anzahl = returnQueryParams()[1];
+createAnzahlPTags(anzahl);
+reload(() => callbackFunction(url, anzahl));
---
+++ toFinish/toFinish.js
@@ -0,0 +1,69 @@
+//Tags that display the wanted data
+
+//Racemap test Id --> 61a21210e5d45d000199f7d9
+
+const reload = (callback) => {
+  //Ruft die Funktion immer wieder aller 1 Sekunde auf
+  setInterval(callback, 1000);
+};
+
+const returnEventID = () => {
+  const Location = document.location;
+  //returns the id as an object of this
+  const queryParams = new URLSearchParams(Location.search);
+
+  let eventId = "";
+
+  if (queryParams != null) {
+    for (const [key, value] of queryParams) {
+      //Here will be a few other security tags
+      if (key == "eventId") {
+        eventId = value;
+      } else {
+        alert("Misinput at the query Params");
+      }
+    }
+  }
+
+  return eventId;
+};
+
+const createUrl = (eventId) => {
+  //scheme: https://racemap.com/api/data/v1/:eventId/current
+  return `https://racemap.com/api/data/v1/${eventId}/current`;
+};
+
+const requestData = async (url) => {
+  //
+
+  const request = await fetch(url);
+  const toJSon = await request.json();
+  let distanceArray = [];
+  for (value of toJSon.starters) {
+    if (value.current != null) {
+      distanceArray.push(value.current.toFinish);
+    }
+  }
+  let smallestDistance = distanceArray[0];
+  for (value in distanceArray) {
+    if (value < smallestDistance) {
+      value = smallestDistance;
+    }
+  }
+  console.log(smallestDistance);
+
+  return smallestDistance;
+};
+
+const modifyDataTags = (doubleValue) => {
+  const tag = document.getElementById("toGo");
+  tag.innerHTML = doubleValue;
+};
+
+const callbackFunction = (urlCreate) => {
+  requestData(urlCreate).then((value) => modifyDataTags(value));
+};
+
+const urlCreate = createUrl(returnEventID());
+
+reload(() => callbackFunction(urlCreate));
